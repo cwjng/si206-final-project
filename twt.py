a21cc7306f290dfff3da224e164bf3a1813368d6
cwjng <97417286+cwjng@users.noreply.github.com>
2023-04-21 12:41:39 -0400

    Add files via upload

--- twt.py
+++ twt.py
@@ -31,12 +31,12 @@ c = conn.cursor()
 last_artist_name = ''
 # Initialize the counter
 artist_count = 0
-
+conn.close()
 
 # Loop until all artists are searched
 while True:
     # Get 25 distinct artists from the database, starting from the last searched artist
-    artists = c.execute(f"SELECT DISTINCT name FROM top_artists WHERE name > '{last_artist_name}' LIMIT 25").fetchall()
+    artists = c.execute(f"SELECT DISTINCT name FROM top_artists LIMIT 25").fetchall()
 
     # Stop the loop if no more artists are found
     if not artists:
@@ -44,48 +44,52 @@ while True:
 
     # Loop through artists and count tweets, retweets and mentions
     for artist in artists:
-        # Construct query string for artist search
-        query = f"{artist[0]} OR @{artist[0]} since:{(datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d')}"
-
-        # Get tweets containing the artist name
-        while True:
-            try:
-                tweets = tweepy.Cursor(api.search_tweets, q=query, tweet_mode='extended').items(66)
-                break
-            except tweepy.TooManyRequests as e:
-                time.sleep(60*15) # wait 15 minutes before trying again
-
-        # Count tweets, retweets and mentions
-        tweet_count = 0
-        retweet_count = 0
-        mention_count = 0
-        print('Searching for tweets for ' + artist[0])
-        for tweet in tweets:
-            # Check if the artist name is mentioned in the tweet text or full text
-            if artist[0].lower() in tweet.full_text.lower():
-                tweet_count += 1
-            # Check if the tweet is a retweet and increment retweet count
-            if hasattr(tweet, 'retweeted_status'):
-                retweet_count += 1
-            # Check if the tweet mentions another user and increment mention count
-            if tweet.entities['user_mentions']:
-                mention_count += 1
-
-        # Calculate the total engagement count
-        total_engagements = tweet_count + retweet_count + mention_count
-
-        # Update artist record in database
-        c.execute("UPDATE top_artists SET tweets=?, retweets=?, mentions=?, total_engagements=? WHERE name=?",
-                (tweet_count, retweet_count, mention_count, total_engagements, artist[0]))
-        conn.commit()
-        print("Changes committed")
         artist_count += 1
         print(artist_count)
-        # Set the last searched artist name to the current artist
-        last_artist_name = artist[0]
+        if artist_count > 25:
+            break
+        else:
+            # Construct query string for artist search
+            query = f"{artist[0]} OR @{artist[0]} since:{(datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d')}"
 
-        # Sleep for a short period to avoid hitting Twitter API rate limits
-        time.sleep(1)
-print('Last artist is ' + last_artist_name)
-# Close database connection
-conn.close()
+            # Get tweets containing the artist name
+            while True:
+                try:
+                    tweets = tweepy.Cursor(api.search_tweets, q=query, tweet_mode='extended').items(66)
+                    break
+                except tweepy.TooManyRequests as e:
+                    time.sleep(60*15) # wait 15 minutes before trying again
+
+            # Count tweets, retweets and mentions
+            tweet_count = 0
+            retweet_count = 0
+            mention_count = 0
+            print('Searching for tweets for ' + artist[0])
+            for tweet in tweets:
+                # Check if the artist name is mentioned in the tweet text or full text
+                if artist[0].lower() in tweet.full_text.lower():
+                    tweet_count += 1
+                # Check if the tweet is a retweet and increment retweet count
+                if hasattr(tweet, 'retweeted_status'):
+                    retweet_count += 1
+                # Check if the tweet mentions another user and increment mention count
+                if tweet.entities['user_mentions']:
+                    mention_count += 1
+
+            # Calculate the total engagement count
+            total_engagements = tweet_count + retweet_count + mention_count
+
+            # Update artist record in database
+            c.execute("UPDATE top_artists SET tweets=?, retweets=?, mentions=?, total_engagements=? WHERE name=?",
+                    (tweet_count, retweet_count, mention_count, total_engagements, artist[0]))
+            conn.commit()
+            print("Changes committed")
+
+            # Set the last searched artist name to the current artist
+            last_artist_name = artist[0]
+
+            # Sleep for a short period to avoid hitting Twitter API rate limits
+            time.sleep(1)
+            print('Last artist is ' + last_artist_name)
+    # Close database connection
+    conn.close()

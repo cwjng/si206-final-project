14397de70af3e576e7ff2000b3279d8c1c14f283
yuyuuyang <112489804+yuyuuyang@users.noreply.github.com>
2023-04-21 20:41:36 -0400

    Add files via upload

--- spotify.py
+++ spotify.py
@@ -17,7 +17,8 @@ c = conn.cursor()
 
 # Create the "top_artists" table if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS top_artists
-             (id TEXT PRIMARY KEY,
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+              artist_id TEXT NOT NULL,
               name TEXT NOT NULL,
               popularity INTEGER NOT NULL,
               followers INTEGER)''')
@@ -25,7 +26,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS top_artists
 # Create the "song_info" table if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS song_info
              (id INTEGER PRIMARY KEY,
-              artist_id TEXT NOT NULL,
+              artist_id INTEGER NOT NULL,
               name TEXT NOT NULL,
               top_song TEXT NOT NULL,
               popularity INTEGER NOT NULL,
@@ -34,6 +35,9 @@ c.execute('''CREATE TABLE IF NOT EXISTS song_info
 # Check how many rows are already in the table
 c.execute("SELECT COUNT(*) FROM top_artists")
 num_rows = c.fetchone()[0]
+
+c.execute("SELECT COUNT(*) FROM song_info")
+num_rows2 = c.fetchone()[0]
 
 if num_rows < 100:
     # Retrieve the top 25 most streamed artists of 2021
@@ -46,35 +50,44 @@ if num_rows < 100:
         artist_popularity = item['popularity']
         artist_info = sp.artist(artist_id)
         followers = artist_info['followers']['total']
-        c.execute("INSERT OR IGNORE INTO top_artists (id, name, popularity, followers) VALUES (?, ?, ?, ?)",
+
+        c.execute("INSERT OR IGNORE INTO top_artists (artist_id, name, popularity, followers) VALUES (?, ?, ?, ?)",
                   (artist_id, artist_name, artist_popularity, followers))
-        
-        top_tracks = sp.artist_top_tracks(artist_id)
-        top_track = top_tracks['tracks'][0]
-        top_track_name = top_track['name']
-        top_track_popularity = top_track['popularity']
-        c.execute("INSERT INTO song_info (artist_id, name, top_song, popularity) VALUES (?, ?, ?, ?)",
-                  (artist_id, artist_name, top_track_name, top_track_popularity))
+        artist_row_id = c.lastrowid
+
+        # Check if the number of rows in song_info is less than 100 before inserting data
+        if num_rows2 < 100:
+            top_tracks = sp.artist_top_tracks(artist_id)
+            top_track = top_tracks['tracks'][0]
+            top_track_name = top_track['name']
+            top_track_popularity = top_track['popularity']
+
+            c.execute("INSERT INTO song_info (artist_id, name, top_song, popularity) VALUES (?, ?, ?, ?)",
+                      (artist_row_id, artist_name, top_track_name, top_track_popularity))
+            num_rows2 += 1
+        else:
+            break
+
+        num_rows += 1
 
     # Commit the changes to the database and close the connection
     conn.commit()
     conn.close()
 
-    # Wait for a few seconds before running the code again
-    time.sleep(5)
 
-# Connect to the SQLite database
-conn = sqlite3.connect('top_artists.db')
-c = conn.cursor()
 
-# Select all rows from the top_artists table
-c.execute('SELECT * FROM top_artists')
-rows = c.fetchall()
+# # Connect to the SQLite database
+# conn = sqlite3.connect('top_artists.db')
+# c = conn.cursor()
 
-# Print the results
-for row in rows:
-    print(row)
+# # Select all rows from the top_artists table
+# c.execute('SELECT * FROM top_artists')
+# rows = c.fetchall()
 
-# Close the connection
-conn.close()
+# # Print the results
+# for row in rows:
+#     print(row)
+
+# # Close the connection
+# conn.close()
 
